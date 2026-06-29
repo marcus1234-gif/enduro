640c3da0d7e4d5afbfc06fce7d7f75e719499042
marcus1234-gif <bartolome.marcus@gmail.com>
2026-06-29 12:31:46 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Service worker: copies the app into the phone's storage so it opens offline.
 // Bump CACHE to a new value whenever you change the app to force a refresh.
-const CACHE = "enduro-v13";
+const CACHE = "enduro-v14";
 const ASSETS = [
   "./",
   "./index.html",
